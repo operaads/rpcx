eb6ccb1308f028d92fd42e4d13ebd7d262423151
hongxingp <hongxingp@opera.com>
2024-05-21 14:08:05 +0800

    ADX-12984: [test] rpcx client load balance by latency

--- client/selector_test.go
+++ client/selector_test.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"github.com/stretchr/testify/assert"
+	"sort"
 	"testing"
 )
 
@@ -34,5 +36,126 @@ func Test_consistentHashSelector_UpdateServer(t *testing.T) {
 	s.UpdateServer(servers)
 	if len(s.h.All()) != len(servers) {
 		t.Errorf("UpdateServer: expected %d server but got %d", len(servers), len(s.h.All()))
+	}
+}
+
+func Test_createLatencyWeighted_UpdateServer(t *testing.T) {
+	testCases := []struct {
+		name    string
+		servers map[string]string
+		results []*Weighted
+	}{
+		{
+			name: "test_all_empty",
+			servers: map[string]string{
+				"tcp@192.168.1.16:9392": "",
+				"tcp@192.168.1.16:9393": "",
+				"tcp@192.168.1.16:9394": "",
+			},
+			results: []*Weighted{
+				{
+					Server:          "tcp@192.168.1.16:9392",
+					Weight:          100,
+					EffectiveWeight: 100,
+				},
+				{
+					Server:          "tcp@192.168.1.16:9393",
+					Weight:          100,
+					EffectiveWeight: 100,
+				},
+				{
+					Server:          "tcp@192.168.1.16:9394",
+					Weight:          100,
+					EffectiveWeight: 100,
+				},
+			},
+		},
+		{
+			name: "test_all_equal",
+			servers: map[string]string{
+				"tcp@192.168.1.16:9392": "latency=30",
+				"tcp@192.168.1.16:9393": "latency=30",
+				"tcp@192.168.1.16:9394": "latency=30",
+			},
+			results: []*Weighted{
+				{
+					Server:          "tcp@192.168.1.16:9392",
+					Weight:          100,
+					EffectiveWeight: 100,
+				},
+				{
+					Server:          "tcp@192.168.1.16:9393",
+					Weight:          100,
+					EffectiveWeight: 100,
+				},
+				{
+					Server:          "tcp@192.168.1.16:9394",
+					Weight:          100,
+					EffectiveWeight: 100,
+				},
+			},
+		},
+		{
+			name: "test_compound_1",
+			servers: map[string]string{
+				"tcp@192.168.1.16:9392": "latency=60",
+				"tcp@192.168.1.16:9393": "latency=40",
+				"tcp@192.168.1.16:9394": "",
+			},
+			results: []*Weighted{
+				{
+					Server:          "tcp@192.168.1.16:9392",
+					Weight:          83,
+					EffectiveWeight: 83,
+				},
+				{
+					Server:          "tcp@192.168.1.16:9393",
+					Weight:          125,
+					EffectiveWeight: 125,
+				},
+				{
+					Server:          "tcp@192.168.1.16:9394",
+					Weight:          100,
+					EffectiveWeight: 100,
+				},
+			},
+		},
+		{
+			name: "test_compound_2",
+			servers: map[string]string{
+				"tcp@192.168.1.16:9392": "latency=60",
+				"tcp@192.168.1.16:9393": "latency=40",
+				"tcp@192.168.1.16:9394": "latency=50",
+			},
+			results: []*Weighted{
+				{
+					Server:          "tcp@192.168.1.16:9392",
+					Weight:          83,
+					EffectiveWeight: 83,
+				},
+				{
+					Server:          "tcp@192.168.1.16:9393",
+					Weight:          125,
+					EffectiveWeight: 125,
+				},
+				{
+					Server:          "tcp@192.168.1.16:9394",
+					Weight:          100,
+					EffectiveWeight: 100,
+				},
+			},
+		},
+	}
+
+	s := newWeightedLatencySelector(nil).(*weightedLatencySelector)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			s.UpdateServer(tc.servers)
+			sort.Slice(s.servers, func(i, j int) bool {
+				return s.servers[i].Server < s.servers[j].Server
+			})
+			assert.Equal(t, tc.results, s.servers)
+		})
 	}
 }
